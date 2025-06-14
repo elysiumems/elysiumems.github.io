fe99b5ac78808561d127d601feb30d8ba353566a
elysiumems <cermakdavid212@gmail.com>
2025-06-14 17:55:58 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+// Ovaj fajl nije potreban za GitHub Pages, ali možeš ga koristiti za lokalnu razvojnu verziju.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
